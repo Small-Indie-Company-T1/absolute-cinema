ce0f383a27aa6b9ee4a6e27351e95f1418aca59f
Kirill <kirka1408kirka@gmail.com>
2026-03-24 12:08:10 +0300

    Update security settings

---
+++ src/config/components/security.py
@@ -0,0 +1,9 @@
+import os
+
+ALLOWED_HOSTS = []
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret-key-placeholder")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
