cb198cb7613918c661649439c280da5d58b08ad1
Kirill <kirka1408kirka@gmail.com>
2026-04-27 22:57:07 +0300

    Add custom exceptions

--- src/interactions/services/services.py
+++ src/interactions/services/services.py
@@ -1,19 +1,17 @@
 from django.db import IntegrityError
-from ..models import Watchlist
 from catalog.models import Movie
+from ..domain.models import Watchlist
+from ..domain.exceptions import MovieNotFound, MovieAlreadyInWatchlist
 
 def add_movie_to_watchlist(user, movie_id):
     try:
         movie = Movie.objects.get(id=movie_id)
     except Movie.DoesNotExist:
-        return None, "Фильм не найден."
+        raise MovieNotFound("Фильм не найден")
 
-    try:
-        watchlist_entry, created = Watchlist.objects.get_or_create(user=user, movie=movie)
+    watchlist_entry, created = Watchlist.objects.get_or_create(user=user, movie=movie)
 
-        if created:
-            return watchlist_entry, None
-        else:
-            return None, "Фильм уже есть в списке"
-    except IntegrityError:
-        return None, "Ошибка при добавлении фильма в список"
+    if created:
+        return watchlist_entry
+    else:
+        raise MovieAlreadyInWatchlist("Фильм уже в списке просмотра")
