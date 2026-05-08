f4e35fd7d9694bc3b3b847e4634b4ec1296bd503
Вячеслав Смирнов <smslava7@gmail.com>
2026-05-08 15:33:51 +0300

    watchlist admin fix

--- src/interactions/admin.py
+++ src/interactions/admin.py
@@ -6,6 +6,6 @@ from .domain.models import Watchlist
 @admin.register(Watchlist)
 class WatchlistAdmin(admin.ModelAdmin):
     list_display = ("user", "movie", "watched", "added_at")
-    search_fields = ("user__username", "movie__title")
+    search_fields = ("user__email", "movie__title")
     list_filter = ("added_at", "watched")
     ordering = ("-added_at",)
