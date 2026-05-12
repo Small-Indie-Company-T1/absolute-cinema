1606f15f0018ce244b3923ba61736e2809ae28c3
Вячеслав Смирнов <smslava7@gmail.com>
2026-05-12 16:53:58 +0300

    добавил сыроватую логику в модель фильма

--- django_project/catalog/admin.py
+++ django_project/catalog/admin.py
@@ -10,7 +10,21 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "release_date", "duration"]
-    list_filter = ["release_date", "genre"]
+    list_display = ["id", "title", "is_free", "release_date", "duration"]
+    list_filter = ["is_free", "available_in_plans", "release_date", "genre"]
     search_fields = ["title", "description"]
-    filter_horizontal = ["genre"]
+    filter_horizontal = ["genre", "available_in_plans"]
+
+    fieldsets = (
+        (
+            None, {
+                'fields': ('title', 'description', 'release_date', 'duration', 'genre')
+            }
+        ),
+        (
+            'Доступность', {
+                'fields': ('is_free', 'available_in_plans'),
+                'description': ('Если флаг is_free не установлен, выберите планы подписок')
+            }
+        ),
+    )
