a993c5c2594b34c6754e9f68cbe7d2c9c7247d4d
Kirill <kirka1408kirka@gmail.com>
2026-05-15 23:11:47 +0300

    Update config

--- fastapi_service/app/core/config.py
+++ fastapi_service/app/core/config.py
@@ -1,7 +1,18 @@
-import os
+from pydantic_settings import BaseSettings
 
 
-class Settings:
-    BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+class Settings(BaseSettings):
+    jwt_secret: str = "secret-key"
+    jwt_algorithm: str = "HS256"
+    access_token_expire_minutes: int = 2
+
+    django_api_url: str = "http://localhost:8000/api"
+
+    cors_origins: list = ["http://localhost:8000", "http://127.0.0.1:8000"]
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
+
 
 settings = Settings()
