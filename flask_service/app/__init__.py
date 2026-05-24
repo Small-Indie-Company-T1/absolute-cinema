5508bee912e021e6460a0d37b6dad39cea648dc0
Вячеслав Смирнов <smslava7@gmail.com>
2026-05-24 19:28:39 +0300

    added db table and migration

--- flask_service/app/__init__.py
+++ flask_service/app/__init__.py
@@ -3,15 +3,23 @@ from flask import Flask
 from .config import Config
 from .api.routes import api_bp
 from .errors.handlers import register_error_handlers
+from .extensions import db, migrate
 
 
 def register_blueprints(app: Flask):
     app.register_blueprint(api_bp, url_prefix="/api/v1")
 
+def register_extensions(app: Flask):
+    db.init_app(app)
+    migrate.init_app(app, db)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    register_extensions(app)
+
+    from app.models import Review
+
     register_blueprints(app)
     register_error_handlers(app)
 
--- flask_service/app/models/review.py
+++ flask_service/app/models/review.py
@@ -1,4 +1,37 @@
-from extensions import db
+from datetime import datetime, timezone
 
+from app.extensions import db
 
-class Review(db.Model): ...
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    status = db.Column(
+        db.String(16),
+        nullable=False,
+        default='pending',
+        server_default='pending'
+    )
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc),
+        server_default=db.func.now()
+    )
+
+    def to_dict(self) -> dict:
+        return {
+            'id': self.id,
+            'movie_id': self.movie_id,
+            'user_id': self.user_id,
+            'rating': self.rating,
+            'text': self.text,
+            'status': self.status,
+            'created_at': self.created_at.isoformat() + 'Z'
+            if self.created_at is not None
+            else None,
+        }
